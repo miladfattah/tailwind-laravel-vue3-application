08217ec4ef23a8f87dcf7d4f095823430766e788
MiladFattah <miladalpha1379@gmail.com>
2022-05-19 17:56:42 +0430

    statrt project with vue and laravel

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script >
+
+
+import {mapState} from "vuex"
+
+export default {
+  computed : {
+    ...mapState(['user'])
+  }
+}
+</script>
+
+<template>
+  <h1>Hello</h1>
+  <h1 class="text-3xl font-bold underline text-gray-300">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+  state : {
+    user : {
+      data : {
+        name : "zak"
+      },
+      token : null
+    }
+  } ,
+  getters : {
+
+  },
+  actions : {
+
+  },
+  mutations : {
+
+  },
+  modules : {
+
+  }
+})
+
+
+export default store ;
